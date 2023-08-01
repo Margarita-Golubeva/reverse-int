723d557fabea151c9028ad66f093b9aff092bb87
Margarita Golubeva <margaret.golubeva@mail.ru>
2023-08-02 00:48:36 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+    let str = n.toString();
+    let strMod = str.replace('-', '');
+    let result = '';
+
+    for (let i = 0; i <= strMod.length - 1; i++) {
+        result = `${strMod[i]}${result}`;
+    }
+    return result;
+    }
+
